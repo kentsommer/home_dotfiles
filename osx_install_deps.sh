569a3b6a989479a9026d1338a682969c2e752c02
Kent Sommer <work@kentsommer.com>
2026-01-05 21:38:47 -0800

    Update osx deps installer

--- osx_install_deps.sh
+++ osx_install_deps.sh
@@ -1,2 +1,38 @@
 #!/usr/bin/env bash
-brew bundle -g
+brew bundle --file=- <<EOF
+tap 'FelixKratz/formulae'
+tap 'nikitabobko/tap'
+
+brew 'ninja'
+brew 'cmake'
+brew 'gettext'
+brew 'curl'
+brew 'git'
+cask 'ghostty'
+brew 'fish'
+brew 'FelixKratz/formulae/borders'
+brew 'mpv'
+cask 'font-fira-code-nerd-font'
+cask 'font-noto-color-emoji'
+brew 'stow'
+brew 'less'
+brew 'rustup'
+brew 'btop'
+brew 'lsd'
+brew 'neovim', args: ["HEAD"]
+brew 'jj'
+brew 'yazi'
+brew 'zellij'
+brew 'fastfetch'
+brew 'fzf'
+cask '1password-cli'
+cask 'nikitabobko/tap/aerospace'
+cask 'alfred'
+EOF
+
+if [ $? -eq 0 ]; then
+    echo "✅ Brew bundle completed successfully!"
+else
+    echo "❌ Brew bundle failed."
+    exit 1
+fi
